6167d65ec53cd916e346a4262f47365bf7633724
Tyulnev <tyulnevi@win.desy.de>
2019-08-30 16:54:09 +0200

    peak intensity

--- help_functions/do_FourierSplitStep2D.m
+++ help_functions/do_FourierSplitStep2D.m
@@ -1,8 +1,7 @@
-%Calculate propagated electric field after distance Lz via fourier split
-%step
+%Calculate propagated electric field after distance Lz via Rk with FT
 %d/dz(E) from SVEA in 1D
-%Implemented Effects: GVD, SPM, Attenuation, Ionization
-function [Erf,matTprop,zprop,dQhist,whist]=do_FourierSplitStep2D(mesh,beam,medium,pulse,boundary,comment)
+%Implemented Effects: GVD, SPM, Attenuation, Ionization(Defocusing & Loss)
+function [Erf,matTprop,zprop,Qhist,whist,IonizLvl]=do_FourierSplitStep2D(mesh,beam,medium,pulse,boundary,comment)
 
 rayl=calc_zrayleigh(beam,mesh,pulse,0);
 wprop=calc_wprop(rayl.win,rayl.zr,rayl.zr);
@@ -15,10 +14,14 @@ matTprop=trapz(mesh.t,medium.Iconst.*abs(pulse.Ert).^2,2);
 counter=0;
 h=mesh.dz;
 zprop=0;
-dQhist=0;
+Qhist=pulse.Energy;
 m=0;
 whist=mesh.dr*2+(find(abs(pulse.Erf(:,pulse.pfmid)).^2<abs(pulse.Erf(1,pulse.pfmid))^2/exp(2),1)-1)*mesh.dr;
+[n_e,Eg,n_gas]=calc_2DeDensityADK(pulse.Ert,mesh,medium,beam,pulse);
+IonizLvl=max(n_e(1,:))/n_gas;
+
     while zprop<(mesh.Lz)
+%%        
         m=m+1;
             %SST+SPM+DIV+ION via Runge Kutta
             [Erf]=do_LowStoreRK(mesh,pulse,beam,medium,Erf,M_fd,h);
@@ -30,14 +33,14 @@ whist=mesh.dr*2+(find(abs(pulse.Erf(:,pulse.pfmid)).^2<abs(pulse.Erf(1,pulse.pfm
             counter=counter+1;
             zprop=zprop+h;
             if counter==1
-                Ert=myifft(Erf,mesh);   
+                Ert=myifft(Erf,mesh); 
                 matTprop=[matTprop,trapz(mesh.t,medium.Iconst.*abs(Ert).^2,2)];
                 condition3=sum(sum(isinf(matTprop),1),2)>=1;
                 test_errorMSG(condition3,'Error in Ert: Inf values')
                 counter=0;
                 Q=2*pi.*trapz(mesh.r,transpose(mesh.r).*matTprop(:,end));
                 dQ=(pulse.Energy-Q)/abs(pulse.Energy);
-                dQhist=[dQhist,dQ];
+                Qhist=[Qhist,Q];
                 waist=mesh.dr*2+(find(abs(Erf(:,pulse.pfmid)).^2<abs(Erf(1,pulse.pfmid))^2/exp(2),1)-1)*mesh.dr;
                 whist=[whist,waist];
                 if strcmp(globproperties.mode,'debug')
@@ -46,16 +49,13 @@ whist=mesh.dr*2+(find(abs(pulse.Erf(:,pulse.pfmid)).^2<abs(pulse.Erf(1,pulse.pfm
                     title(['z=',num2str(zprop*1000),'mm','  ','Qout=',num2str(Q*1000),'mJ','  ','dQ=',num2str(dQ)]);
                     pause(0.1);
                 end
+                %% Ionization level
+                [n_e]=calc_2DeDensityADK(Ert,mesh,medium,beam,pulse);
+                IonizLvl=[IonizLvl,max(n_e(1,:))/n_gas];
             end                    
-            %% plot
-%             Qout=2*pi.*trapz(mesh.r,transpose(mesh.r).*trapz(mesh.f,medium.Iconst.*abs(Erf).^2,2),1);
-%             plot(mesh.r,[abs(pulse.Ert(:,mesh.tmid)).^2,abs(Ert(:,mesh.tmid)).^2])
-%             plot(mesh.f,[abs(pulse.Erf(1,:)).^2;abs(Erf(1,:)).^2]); xlim([-3e14 3e14]) 
-%             plot(mesh.t,[abs(pulse.Ert(1,:)).^2;abs(Ert(1,:)).^2]); xlim([-1e-13 1e-13])
-%             imagesc(mesh.f,mesh.r,abs(Erf).^2); xlim([1e14 6e14])       
-%             imagesc(mesh.t,mesh.r,abs(myifft(Erf,mesh)).^2); xlim([-1e-13 1e-13])   
-%             title(['z=',num2str(zprop)]);
-%             pause(0.1)
+            
+            
+%% Tests
             if strcmp(lastwarn,'myErrorID')
                 switch globproperties.mode
                     case 'debug'
